a3bf650f25bf3ad04c0e53ae94fba1cce7bae571
Hamza FELLAH <fellahhamza48@gmail.com>
2024-11-21 20:34:02 +0100

    FriendRequest Done

--- src/main/java/com/fssm/ChatApp/Controller/FriendRequestController.java
+++ src/main/java/com/fssm/ChatApp/Controller/FriendRequestController.java
@@ -1,10 +1,12 @@
 package com.fssm.ChatApp.Controller;
 
+import com.fssm.ChatApp.Model.Status;
 import com.fssm.ChatApp.Service.FriendRequestService;
 import com.fssm.ChatApp.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +27,19 @@ public class FriendRequestController {
             throw new RuntimeException("Failed to send request: "+ e.getMessage());
         }
     }
+
+    // This function takes as a parameter a specific friendrequest id to update his status from PENDING
+    // TO ACCEPTED or REJECTED based on the friend action
+    @PutMapping("/ChangeRequestStatus/{friendRequestId}/{status}")
+    public String updateFriendRequestStatus(@PathVariable Integer friendRequestId, @PathVariable Status status){
+        try{
+            friendRequestService.updateFriendRequestStatus(friendRequestId,status);
+            return "Friend request sent successfully!";
+        }catch (Exception e){
+            // In case there's an error we throw an exception
+            throw new RuntimeException("Failed to update status: "+ e.getMessage());
+        }
+    }
+
 
 }
